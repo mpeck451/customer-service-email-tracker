3821b000a92b616ad89106a16ef8ab3559f2e16a
Mason Peck <mpeck451@outlook.com>
2022-12-23 09:59:19 -0600

    Add cvs reader and save data to variable.

--- src/scripts/main-script.py
+++ src/scripts/main-script.py
@@ -1,5 +1,22 @@
-
+import csv
+import sqlite3
 
 print("Script Start...")
+csv_data = []
+
+print("Connecting to sqlite3 database...")
+
+print("Opening csv file...")
+with open('../../../database/customer-emails.csv') as customer_emails:
+    file_reader = csv.DictReader(customer_emails)
+    csv_data = list(file_reader)
+for row in csv_data:
+    print(row)
+master_fields = list(csv_data[0].keys())
+print("Updating database tables...")
+
+print("Closing database connection...")
+
+print("Generating reports...")
 
 print("...Script End")
