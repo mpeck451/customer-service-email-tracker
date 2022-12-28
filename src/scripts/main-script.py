5e4937ec51cd09d87f89bd9a42407a9f67cbbd4c
Mason Peck <mpeck451@outlook.com>
2022-12-28 10:57:46 -0600

    Add department data csv reader.

--- src/scripts/main-script.py
+++ src/scripts/main-script.py
@@ -2,17 +2,25 @@ import csv
 import sqlite3
 
 print("Script Start...")
-csv_data = []
+csv_email_data = []
+csv_department_data = []
+
+print("Opening csv files...")
+with open('../../../database/implementation-department.csv') as department_data:
+    file_reader = csv.DictReader(department_data)
+    csv_department_data = list(file_reader)
+for row in csv_department_data:
+    print(row)
+with open('../../../database/customer-emails.csv') as customer_emails:
+    file_reader = csv.DictReader(customer_emails)
+    csv_email_data = list(file_reader)
+for row in csv_email_data:
+    print(row)
+master_fields = list(csv_email_data[0].keys())
+print(master_fields)
 
 print("Connecting to sqlite3 database...")
 
-print("Opening csv file...")
-with open('../../../database/customer-emails.csv') as customer_emails:
-    file_reader = csv.DictReader(customer_emails)
-    csv_data = list(file_reader)
-for row in csv_data:
-    print(row)
-master_fields = list(csv_data[0].keys())
 print("Updating database tables...")
 
 print("Closing database connection...")
